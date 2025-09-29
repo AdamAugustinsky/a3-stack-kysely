4c49a33906c51b7a9c67cd64bb749a5526f7ffe5
Adam Axel Augustinsky <AdamAugustinsky@gmail.com>
2025-09-29 00:58:07 -0300

    add some missing http methods

--- src/routes/api/[...slugs]/+server.ts
+++ src/routes/api/[...slugs]/+server.ts
@@ -4,5 +4,8 @@ type RequestHandler = (v: { request: Request }) => Response | Promise<Response>;
 
 export const GET: RequestHandler = ({ request }) => app.handle(request);
 export const POST: RequestHandler = ({ request }) => app.handle(request);
+export const PUT: RequestHandler = ({ request }) => app.handle(request);
+export const HEAD: RequestHandler = ({ request }) => app.handle(request);
 export const PATCH: RequestHandler = ({ request }) => app.handle(request);
+export const OPTIONS: RequestHandler = ({ request }) => app.handle(request);
 export const DELETE: RequestHandler = ({ request }) => app.handle(request);
